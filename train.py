3419c70e09f8bc701e06157e00c3d9a9cd633fc0
bryanadamg <bryanadamg>
2024-02-16 15:18:38 +0800

    fix: visdom server

--- train.py
+++ train.py
@@ -76,7 +76,7 @@ if __name__ == '__main__':
             if total_iters % opt.print_freq == 0:    # print training losses and save logging information to the disk
                 losses = model.get_current_losses()
                 visualizer.print_current_losses(epoch, epoch_iter, losses, optimize_time, t_data)
-                if opt.display_id > 0:
+                if opt.display_id is None or opt.display_id > 0:
                     visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, losses)
 
             if total_iters % opt.save_latest_freq == 0:   # cache our latest model every <save_latest_freq> iterations
