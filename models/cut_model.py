0733c60c5b9590185ecc8df3da4317a9b44a0757
bryanadamg <bryanadamg@datasintesa.id>
2024-02-29 01:26:21 +0800

    patch: add pred_B loss to plot

--- models/cut_model.py
+++ models/cut_model.py
@@ -59,7 +59,7 @@ class CUTModel(BaseModel):
 
         # specify the training losses you want to print out.
         # The training/test scripts will call <BaseModel.get_current_losses>
-        self.loss_names = ['G_GAN', 'D_real', 'D_fake', 'G', 'NCE']
+        self.loss_names = ['G_GAN', 'D_real', 'D_fake', 'G', 'NCE', 'pred_B']
         self.visual_names = ['real_A', 'fake_B', 'real_B']
         self.nce_layers = [int(i) for i in self.opt.nce_layers.split(',')]
 
