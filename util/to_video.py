e3adcda5af37102f820e74b7cb86a9ac23280aa9
bryanadamg <bryanadamg@datasintesa.id>
2024-03-08 17:30:29 +0800

    add: frames to video script

---
+++ util/to_video.py
@@ -0,0 +1,26 @@
+import cv2
+import os
+
+
+# Open the video file
+input_folder = './results/x/test_latest/images/fake_B'
+real_folder = './results/x/test_latest/images/real_A'
+
+output_folder = './results/utopilot_sun2rain/test_latest'
+os.makedirs(output_folder, exist_ok=True)
+
+
+B_images = os.listdir(input_folder)
+A_images = os.listdir(real_folder)
+
+
+frame_0 = cv2.imread(os.path.join(input_folder, B_images[0]))
+height, width, layers = frame_0.shape
+video = cv2.VideoWriter(f'{output_folder}/video.avi', 0, 2, (width*2,height))
+for frame in sorted(B_images):
+    fb = cv2.imread(f"{input_folder}/{frame}")
+    fa = cv2.imread(f"{real_folder}/{frame}")
+    f = cv2.hconcat([fa, fb])
+    video.write(f)
+
+video.release()
